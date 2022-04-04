5e2dc4afb09c2ebfa5b7435604f8488999febedd
Gavirangaswamy <92967367+Gavirangaswamy@users.noreply.github.com>
2022-04-04 15:16:53 +0530

    Add files via upload

---
+++ spring-mvc2/src/main/java/com/grs/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.grs;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class WelcomeController {
+
+	
+	
+	public WelcomeController() {
+		System.out.println("kjhgfs");
+	}
+}
